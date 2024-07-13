9e1b89e787b9b5a5c59e498a37072518937137e0
Ivan Andraschko <ivan.andraschko@gmail.com>
2024-07-12 23:06:01 -0400

    add tmux conf and cheat sheet

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -6,5 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>cx", "<cmd>!chmod +x %<CR>", { desc = "Make file executable" })
+map("n", "<C-f>", "<cmd>silent !tmux neww fuzzy_finder<CR>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
